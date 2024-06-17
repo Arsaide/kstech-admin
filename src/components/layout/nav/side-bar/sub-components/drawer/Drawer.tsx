b26b66aaa3af12cc56dd22671253366afd397966
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-06-17 17:25:25 +0300

    - worked with styles and UI - added icons

--- src/components/layout/nav/side-bar/sub-components/drawer/Drawer.tsx
+++ src/components/layout/nav/side-bar/sub-components/drawer/Drawer.tsx
@@ -3,36 +3,86 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import { Inbox, Mail } from 'lucide-react';
 import ListItemText from '@mui/material/ListItemText';
+import { NavLink } from 'react-router-dom';
+import { MainColorsEnum } from '../../../../../../lib/enums/colors-enum';
+import { BadgeDollarSign, Home, LayoutList, Plus, Users } from 'lucide-react';
 
 const DrawerElements = (
     <div>
         <Divider />
         <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                <ListItem key={text} disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <Mail /> : <Inbox />}
+            <ListItem
+                disablePadding
+                sx={{ '&:hover': { bgcolor: MainColorsEnum.BLACK02 } }}
+            >
+                <NavLink
+                    to={'/'}
+                    style={{ width: '100%' }}
+                    className={({ isActive }) =>
+                        [isActive ? 'active' : ''].join(' ')
+                    }
+                >
+                    <ListItemButton sx={{ color: MainColorsEnum.BLACK }}>
+                        <ListItemIcon sx={{ color: MainColorsEnum.BLACK }}>
+                            <Home />
                         </ListItemIcon>
-                        <ListItemText primary={text} />
+                        <ListItemText primary={'Главная'} />
                     </ListItemButton>
-                </ListItem>
-            ))}
-        </List>
-        <Divider />
-        <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem key={text} disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <Inbox /> : <Mail />}
+                </NavLink>
+            </ListItem>
+            <ListItem
+                disablePadding
+                sx={{ '&:hover': { bgcolor: MainColorsEnum.BLACK02 } }}
+            >
+                <NavLink to={'/product-list'} style={{ width: '100%' }}>
+                    <ListItemButton sx={{ color: MainColorsEnum.BLACK }}>
+                        <ListItemIcon sx={{ color: MainColorsEnum.BLACK }}>
+                            <LayoutList />
                         </ListItemIcon>
-                        <ListItemText primary={text} />
+                        <ListItemText primary={'Все товары'} />
                     </ListItemButton>
-                </ListItem>
-            ))}
+                </NavLink>
+            </ListItem>
+            <ListItem
+                disablePadding
+                sx={{ '&:hover': { bgcolor: MainColorsEnum.BLACK02 } }}
+            >
+                <NavLink to={'/create-product'} style={{ width: '100%' }}>
+                    <ListItemButton sx={{ color: MainColorsEnum.BLACK }}>
+                        <ListItemIcon sx={{ color: MainColorsEnum.BLACK }}>
+                            <Plus />
+                        </ListItemIcon>
+                        <ListItemText primary={'Создать товар'} />
+                    </ListItemButton>
+                </NavLink>
+            </ListItem>
+            <ListItem
+                disablePadding
+                sx={{ '&:hover': { bgcolor: MainColorsEnum.BLACK02 } }}
+            >
+                <NavLink to={'/analytics'} style={{ width: '100%' }}>
+                    <ListItemButton sx={{ color: MainColorsEnum.BLACK }}>
+                        <ListItemIcon sx={{ color: MainColorsEnum.BLACK }}>
+                            <BadgeDollarSign />
+                        </ListItemIcon>
+                        <ListItemText primary={'Аналитика'} />
+                    </ListItemButton>
+                </NavLink>
+            </ListItem>
+            <ListItem
+                disablePadding
+                sx={{ '&:hover': { bgcolor: MainColorsEnum.BLACK02 } }}
+            >
+                <NavLink to={'/clients'} style={{ width: '100%' }}>
+                    <ListItemButton sx={{ color: MainColorsEnum.BLACK }}>
+                        <ListItemIcon sx={{ color: MainColorsEnum.BLACK }}>
+                            <Users />
+                        </ListItemIcon>
+                        <ListItemText primary={'Клиенты'} />
+                    </ListItemButton>
+                </NavLink>
+            </ListItem>
         </List>
     </div>
 );
