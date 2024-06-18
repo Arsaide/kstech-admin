bf094c771eecc495914533878debbf1253070d30
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-06-18 17:27:39 +0300

    - fixed again and again and again and again

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,10 @@ import ProductListPage from './product-list/page.tsx';
 import AppBarMenu from '../components/layout/nav/AppBar.tsx';
 import SideBar from '../components/layout/nav/side-bar/SideBar.tsx';
 import NotAuthPage from './not-auth/page.tsx';
+import CreateProductPage from './create-product/page.tsx';
+import AnalyticsPage from './analytics/page.tsx';
+import ClientsPage from './clients/page.tsx';
+import ProductIdPage from './product-list/product-id/ProductIdPage.tsx';
 
 function App() {
     const { isLoggedIn, isPending } = useContext(AuthContext);
@@ -26,6 +30,19 @@ function App() {
                             path={'/products-list'}
                             element={<ProductListPage />}
                         />
+                        <Route
+                            path={'/products-list/:id'}
+                            element={<ProductIdPage />}
+                        />
+                        <Route
+                            path={'/create-product'}
+                            element={<CreateProductPage />}
+                        />
+                        <Route
+                            path={'/analytics'}
+                            element={<AnalyticsPage />}
+                        />
+                        <Route path={'/clients'} element={<ClientsPage />} />
                     </Routes>
                 </SideBar>
             ) : (
