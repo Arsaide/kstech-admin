1aac7c0ad5b66d396b0c6b4803263278296b60e0
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-06-19 23:41:57 +0300

    - added swiper - worked with styles

--- src/components/pages/product-list-page/product-id-page/ProductId.tsx
+++ src/components/pages/product-list-page/product-id-page/ProductId.tsx
@@ -6,6 +6,9 @@ import { Alert, Box, CircularProgress } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { MainColorsEnum } from '../../../../utils/enums/colors-enum';
 import ImageGallery from 'react-image-gallery';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import './ProductId.css';
 
 const ProductId = () => {
     const { store } = useContext(Context);
@@ -13,6 +16,7 @@ const ProductId = () => {
     const [images, setImages] = useState<
         { original: string; thumbnail: string }[]
     >([]);
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
     const { isLoading, isError, error, data } = useQuery({
         queryKey: ['product-id', id],
@@ -53,11 +57,75 @@ const ProductId = () => {
                     Error: {error.message}
                 </Alert>
             ) : (
-                <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                    <div style={{ maxWidth: '450px'}}>
-                        <ImageGallery items={images} lazyLoad={true} showNav={false} showPlayButton={false} />
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {/*<div style={{ maxWidth: '450px'}}>*/}
+                    {/*    <ImageGallery items={images} lazyLoad={true} showNav={false} showPlayButton={false} />*/}
+                    {/*</div>*/}
+
+                    <div className={'cnt'}>
+                        <Swiper
+                            style={{
+                                '--swiper-navigation-color': '#fff',
+                                '--swiper-pagination-color': '#fff',
+                            }}
+                            loop={true}
+                            spaceBetween={10}
+                            navigation={true}
+                            thumbs={{ swiper: thumbsSwiper }}
+                            modules={[FreeMode, Navigation, Thumbs]}
+                            className="mySwiper2"
+                        >
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                            </SwiperSlide>
+                        </Swiper>
+                        <Swiper
+                            onSwiper={setThumbsSwiper}
+                            loop={true}
+                            spaceBetween={10}
+                            slidesPerView={4}
+                            freeMode={true}
+                            watchSlidesProgress={true}
+                            modules={[FreeMode, Navigation, Thumbs]}
+                            className="mySwiper"
+                        >
+                            {data &&
+                                data.imgArr.map((img, index) => (
+                                    <SwiperSlide key={index}>
+                                        <img
+                                            src={img}
+                                            alt={'Свайпер тумблер'}
+                                        />
+                                    </SwiperSlide>
+                                ))}
+                        </Swiper>
                     </div>
-                    <div style={{flex: '1 1 auto'}}>
+
+                    <div style={{ flex: '1 1 auto' }}>
                         <div>Name: {data?.name}</div>
                         <div>In availability: {data?.inAvailability}</div>
                         <div>Price: {data?.price}</div>
