778a9cb04d89fc8801da7eae28e87835e211c0a9
Kirill <arsaidekm@gmail.com>
2024-06-29 18:27:56 +0300

    - added change font-size and subscript and superscript buttons - refactoring - updated tscongfig

--- src/components/layout/common/form-inputs/text-editor-input/toolbarOptions.ts
+++ src/components/layout/common/form-inputs/text-editor-input/toolbarOptions.ts
@@ -1,32 +1,51 @@
 export const toolbarOptions = {
-    options: ['inline', 'list', 'link', 'history'],
+    options: ['inline', 'fontSize', 'list', 'link', 'history'],
     inline: {
-        options: ['bold', 'italic', 'underline', 'strikethrough'],
+        options: [
+            'bold',
+            'italic',
+            'underline',
+            'strikethrough',
+            'superscript',
+            'subscript',
+        ],
         bold: {
-            icon: './icons/bold.svg',
+            icon: '../icons/bold.svg',
             title: 'Жирний шрифт',
         },
         italic: {
-            icon: ['./icons/italic.svg'],
+            icon: ['../icons/italic.svg'],
             title: 'Курсивний шрифт',
         },
         underline: {
-            icon: ['./icons/underline.svg'],
+            icon: ['../icons/underline.svg'],
             title: 'Підкреслений шрифт',
         },
         strikethrough: {
-            icon: ['./icons/strikethrough.svg'],
+            icon: ['../icons/strikethrough.svg'],
             title: 'Закреслений шрифт',
         },
+        superscript: {
+            icon: '../icons/superscript.svg',
+            title: 'Верхній індекс',
+        },
+        subscript: {
+            icon: '../icons/subscript.svg',
+            title: 'Нижній індекс',
+        },
+    },
+    fontSize: {
+        icon: ['./icons/fontSize.svg'],
+        options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30],
     },
     list: {
         options: ['unordered', 'ordered'],
         unordered: {
-            icon: './icons/list.svg',
+            icon: '../icons/list.svg',
             title: 'Список з колами',
         },
         ordered: {
-            icon: './icons/listOrdered.svg',
+            icon: '../icons/listOrdered.svg',
             title: 'Список з цифрами',
         },
     },
