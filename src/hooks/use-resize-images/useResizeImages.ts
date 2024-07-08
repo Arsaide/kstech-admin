5d78241e6ea3ab9e9ba89dff167c7b2f3fc4bb0c
Kirill <arsaidekm@gmail.com>
2024-07-08 21:05:24 +0300

    - added new resize image hook

---
+++ src/hooks/use-resize-images/useResizeImages.ts
@@ -0,0 +1,65 @@
+import { ChangeEvent, useState } from 'react';
+import Resizer from 'react-image-file-resizer';
+
+export const useResizeImages = () => {
+    const [originalImage, setOriginalImage] = useState<File | null>(null);
+    const [resizedImage, setResizedImage] = useState<File | null>(null);
+
+    const handleImageChange = (
+        event: ChangeEvent<HTMLInputElement>,
+        onChange: (event: ChangeEvent<HTMLInputElement>) => void,
+    ) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            Resizer.imageFileResizer(
+                file,
+                1080,
+                1230,
+                'webp',
+                100,
+                0,
+                uri => {
+                    if (typeof uri === 'string') {
+                        fetch(uri)
+                            .then(res => res.blob())
+                            .then(blob => {
+                                const formattedFile = new File(
+                                    [blob],
+                                    file.name,
+                                    { type: blob.type },
+                                );
+                                setOriginalImage(formattedFile);
+                            });
+                    }
+                },
+                'base64',
+            );
+
+            Resizer.imageFileResizer(
+                file,
+                126,
+                143.5,
+                'webp',
+                100,
+                0,
+                uri => {
+                    if (typeof uri === 'string') {
+                        fetch(uri)
+                            .then(res => res.blob())
+                            .then(blob => {
+                                const formattedFile = new File(
+                                    [blob],
+                                    file.name,
+                                    { type: blob.type },
+                                );
+                                setResizedImage(formattedFile);
+                            });
+                    }
+                },
+                'base64',
+            );
+        }
+        onChange(event);
+    };
+    return { originalImage, resizedImage, handleImageChange };
+};
