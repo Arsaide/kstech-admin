0dab5db34ef845c0d0e2a7ca92705f2aa0d83191
Kirill <arsaidekm@gmail.com>
2024-07-11 04:02:24 +0300

    - added session time

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Alert, Typography } from '@mui/material';
+import { formatTime } from '../../utils/formatTime';
 
 const HomePage = () => {
-    return <>Auth success :)</>;
+    const remainingTime = parseInt(
+        localStorage.getItem('remainingTime') || '3600',
+        10,
+    );
+
+    return (
+        <>
+            <Alert variant="filled" severity="success">
+                Ви успішно авторизовані в адмін-панелі KS TECH!
+            </Alert>
+            <Typography>
+                Задля безпеки сесія діє {formatTime(remainingTime)} годину!
+            </Typography>
+        </>
+    );
 };
 
 export default HomePage;
--- src/components/layout/nav/AppBar.tsx
+++ src/components/layout/nav/AppBar.tsx
@@ -12,12 +12,18 @@ import { useContext } from 'react';
 import { Context } from '../../../api/context';
 import useAnchorElUser from '../../../hooks/use-anchor-el-user/useAnchorElUser';
 import { AuthContext } from '../../../providers/AuthProvider';
+import { formatTime } from '../../../utils/formatTime';
 
 const AppBarMenu = () => {
     const { store } = useContext(Context);
     const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
     const { anchorElUser, handleOpenUserMenu, handleCloseUserMenu } =
         useAnchorElUser();
+
+    const remainingTime = parseInt(
+        localStorage.getItem('remainingTime') || '3600',
+        10,
+    );
 
     const handleLogout = () => {
         store.logout().then(() => setIsLoggedIn(false));
@@ -44,40 +50,50 @@ const AppBarMenu = () => {
                         KS TECH - Адмін
                     </Typography>
                     {isLoggedIn && (
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton
-                                    onClick={handleOpenUserMenu}
-                                    sx={{ p: 0 }}
-                                >
-                                    <Avatar alt="Admin" src="/admin.jpg" />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'center',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'center',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                <MenuItem onClick={handleCloseUserMenu}>
-                                    <Typography
-                                        textAlign={'center'}
-                                        onClick={handleLogout}
+                        <Box
+                            sx={{
+                                flexGrow: 0,
+                                display: 'flex',
+                                alignItems: 'center',
+                                gap: 1,
+                            }}
+                        >
+                            <Typography>{formatTime(remainingTime)}</Typography>
+                            <Box>
+                                <Tooltip title="Open settings">
+                                    <IconButton
+                                        onClick={handleOpenUserMenu}
+                                        sx={{ p: 0 }}
                                     >
-                                        Log out
-                                    </Typography>
-                                </MenuItem>
-                            </Menu>
+                                        <Avatar alt="Admin" src="/admin.jpg" />
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{ mt: '45px' }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'center',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'center',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    <MenuItem onClick={handleCloseUserMenu}>
+                                        <Typography
+                                            textAlign={'center'}
+                                            onClick={handleLogout}
+                                        >
+                                            Log out
+                                        </Typography>
+                                    </MenuItem>
+                                </Menu>
+                            </Box>
                         </Box>
                     )}
                 </Toolbar>
