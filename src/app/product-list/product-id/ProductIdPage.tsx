16b3e2bd1b3dd0c59809979129e790afb32b5b1b
Kirill <arsaidekm@gmail.com>
2024-07-13 01:02:36 +0300

    - removed alerts

--- src/app/product-list/product-id/ProductIdPage.tsx
+++ src/app/product-list/product-id/ProductIdPage.tsx
@@ -1,13 +1,8 @@
-import { Alert } from '@mui/material';
 import ProductId from '../../../components/pages/product-list-page/product-id-page/ProductId';
 
 const ProductIdPage = () => {
     return (
         <>
-            <Alert severity="warning">In developing...</Alert>
-            <Alert variant="filled" severity="info">
-                Here you are can view or delete or change product
-            </Alert>
             <ProductId />
         </>
     );
