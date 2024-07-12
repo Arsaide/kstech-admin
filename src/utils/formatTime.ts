f1240030688e9edd416d75d87f1029d163ac459b
Kirill <arsaidekm@gmail.com>
2024-07-12 13:42:24 +0300

    - reworked formatTime function - added remaining time toastify message

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,7 +1,27 @@
+import { toast } from 'react-toastify';
+
 export const formatTime = (seconds: number) => {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const secondsLeft = seconds % 60;
-    console.log(hours);
-    return `${hours !== 0 ? `${hours} год.` : ''} ${minutes !== 0 ? `${minutes} хв` : ''}. ${secondsLeft} сeк.`;
+
+    if (minutes == 29 && secondsLeft == 59) {
+        toast.warning('Залишилось 30 хвилин активної сессії!', {
+            autoClose: 15000,
+        });
+    } else if (minutes == 9 && secondsLeft == 59) {
+        toast.warning('Залишилось 10 хвилин активної сессії!', {
+            autoClose: 15000,
+        });
+    } else if (minutes == 4 && secondsLeft == 59) {
+        toast.warning('Залишилось 5 хвилин активної сессії!', {
+            autoClose: 15000,
+        });
+    } else if (minutes == 0 && secondsLeft == 59) {
+        toast.warning('Залишилась 1 хвилин активної сессії!', {
+            autoClose: 15000,
+        });
+    }
+
+    return `${hours !== 0 ? `${hours} год.` : ''} ${minutes !== 0 ? `${minutes} хв.` : ''} ${secondsLeft} сeк.`;
 };
