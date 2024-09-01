88f09b239edb664dda3485f63ae0e3e4627bef87
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-09-02 01:26:01 +0300

    Update index.ts

--- src/api/request/index.ts
+++ src/api/request/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const API_URL = `http://ks-tech.com.ua/api/`;
-export const API_URL = `https://kstech-backend.onrender.com`;
+export const API_URL = `http://ks-tech.com.ua/api/`;
+// export const API_URL = `https://kstech-backend.onrender.com`;
 // export const API_URL = 'http://localhost:8000'
 
 const $api = axios.create({
